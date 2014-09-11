15d9bdb26820154be27b1fb06da87cf3f46b68c7
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-09-11 16:46:40 -0400

    getelements and stringify passing tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,46 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var result = [];
+
+  function getElementClassRecursive(element){
+
+    var returnElement = false;
+
+    if (typeof element === 'undefined'){
+
+      element = document.body;
+
+    }
+
+    if(element.hasOwnProperty('classList')){
+
+      var classList = element.classList;
+
+      for (var i = 0; i < classList.length; i++){
+
+        if (classList[i] === className){
+
+          returnElement = true;
+
+        }
+
+      }
+
+      if (returnElement){ result.push(element) }
+
+      for (var j = 0; j < element.childNodes.length; j++){
+
+        getElementClassRecursive(element.childNodes[j]);
+
+      }
+
+    }
+
+  }
+
+  getElementClassRecursive();
+
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,84 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = "";
+
+  function stringifyJsonRecursively (key) {
+
+    var hasChildren = false;
+
+    if (typeof key === 'undefined'){
+
+      key = obj;
+
+    }
+
+    if (typeof key === 'object' && key !== null){
+
+      if (Array.isArray(key)){
+
+        result += '[';
+
+        for (var i = 0;i < key.length; i++){
+
+          if (typeof key[i] !== 'function' && typeof key[i] !== 'undefined'){
+
+            stringifyJsonRecursively(key[i]);
+
+            result += ',';
+
+            hasChildren = true;
+
+          }
+
+        }
+
+        result = hasChildren ? result.substring(0, result.length - 1) : result;
+
+        result += ']';
+
+      } else {
+
+        result += '{';
+
+        for (var item in key){
+
+          if (key.hasOwnProperty(item) && typeof key[item] !== 'function' && typeof key[item] !== 'undefined'){
+
+            result += '"' + item + '":';
+
+            stringifyJsonRecursively(key[item]);
+
+            result += ',';
+
+            hasChildren = true;
+
+          }
+
+        }
+
+        result = hasChildren ? result.substring(0, result.length - 1) : result;
+
+        result += '}';
+
+      }
+
+    } else if (typeof key !== 'undefined' && typeof key !== 'function'){
+
+      if (typeof key === 'string'){
+
+        key = '"' + key + '"';
+
+      }
+
+      result += key;
+
+    }
+
+  }
+
+  stringifyJsonRecursively();
+
+  return result === ""? undefined : result;
+
 };
